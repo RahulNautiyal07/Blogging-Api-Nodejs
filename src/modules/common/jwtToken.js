38fcaed0ee0a9fee26f9fe67b04b6968a71ce845
rahulnautiyal1871 <rahulnautiyal1871>
2023-04-08 09:46:57 +0530

    optimize documentataion 3

--- src/modules/common/jwtToken.js
+++ src/modules/common/jwtToken.js
@@ -33,7 +33,6 @@ const generateRefreshToken = (payload, expiryTime = refreshToken.time) => {
   return new Promise((resolve, reject) => {
     const secret = refreshToken.secretKey;
     const userId = payload.userId.toString();
-    console.log(userId);
     const options = { expiresIn: expiryTime, issuer: "www.api.com" };
     jwt.sign(payload, secret, options, (err, token) => {
       if (err) return reject(err);
@@ -54,7 +53,6 @@ const verifyRefreshToken = (token) => {
       const userId = payload.userId;
       client.connect();
       client.get(userId).then((result) => {
-        console.log(result, "resuklr");
         if (!result) return reject(err);
         if (token === result) return resolve(userId);
         reject({ message: "Unauthorized request, Please Login again." });
@@ -64,54 +62,6 @@ const verifyRefreshToken = (token) => {
   });
 };
 
-// const signRefreshhToken = (userId) => {
-//   return new Promise((resolve, reject) => {
-//     const payload = {}
-//     const secret = process.env.REFRESH_TOKEN_SECRET
-//     const options = {
-//       expiresIn: '1y',
-//       issuer: 'pickurpage.com',
-//       audience: userId,
-//     }
-//     JWT.sign(payload, secret, options, (err, token) => {
-//       if (err) {
-//         console.log(err.message)
-//         // reject(err)
-//         reject(createError.InternalServerError())
-//       }
-
-//       client.SET(userId, token, 'EX', 365 * 24 * 60 * 60, (err, reply) => {
-//         if (err) {
-//           console.log(err.message)
-//           reject(createError.InternalServerError())
-//           return
-//         }
-//         resolve(token)
-//       })
-//     })
-//   })
-// }
-// const verifyRefreshhToken = (refreshToken) => {
-//   return new Promise((resolve, reject) => {
-//     JWT.verify(
-//       refreshToken,
-//       process.env.REFRESH_TOKEN_SECRET,
-//       (err, payload) => {
-//         if (err) return reject(createError.Unauthorized())
-//         const userId = payload.aud
-//         client.GET(userId, (err, result) => {
-//           if (err) {
-//             console.log(err.message)
-//             reject(createError.InternalServerError())
-//             return
-//           }
-//           if (refreshToken === result) return resolve(userId)
-//           reject(createError.Unauthorized())
-//         })
-//       }
-//     )
-//   })
-// }
 module.exports = {
   generateAccessToken,
   verifyAccessToken,
